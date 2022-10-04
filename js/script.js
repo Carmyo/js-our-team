1ad35f0c7b59ae6fc9a378b5eb81da2aa43d2abc
Carmyo <carmelodicristina@gmail.com>
2022-10-04 14:50:19 +0200

    create staff object and insert them in an array

---
+++ js/script.js
@@ -0,0 +1,45 @@
+console.log('ok js');
+
+const wayneBarnett = {
+    staffName: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    propic: 'wayne-barnett-founder-ceo.jpg',
+};
+const angelaCaroll = {
+    staffName: 'Angela Caroll',
+    role: 'Chief Editor',
+    propic: 'angela-caroll-chief-editor.jpg',
+};
+
+const walterGordon = {
+    staffName: 'Walter Gordon',
+    role: 'Office Manager',
+    propic: 'walter-gordon-office-manager.jpg',
+};
+
+const angelaLopez = {
+    staffName: 'Angela Lopez',
+    role: 'Social Media Manager',
+    propic: 'angela-lopez-social-media-manager.jpg',
+};
+
+const scottEstrada = {
+    staffName: 'Scott Estrada',
+    role: 'Developer',
+    propic: 'scott-estrada-developer.jpg',
+};
+
+const barbaraRamos = {
+    staffName: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    propic: 'barbara-ramos-graphic-designer.jpg',
+};
+
+
+
+
+console.log(angelaLopez.role);
+
+
+const staffInfo = [wayneBarnett, angelaCaroll, walterGordon, angelaLopez, scottEstrada, barbaraRamos];
+console.log(staffInfo);
