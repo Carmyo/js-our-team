528bfc88524327d4143d4b4caf091c6fa8707529
Carmyo <carmelodicristina@gmail.com>
2022-10-04 16:37:30 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -1,45 +1,55 @@
 console.log('ok js');
-
+// di seguito oggetti, uno per membro. Oggetto= variabile con attributi
 const wayneBarnett = {
-    staffName: 'Wayne Barnett',
+    name: 'Wayne Barnett',
     role: 'Founder & CEO',
     propic: 'wayne-barnett-founder-ceo.jpg',
 };
 const angelaCaroll = {
-    staffName: 'Angela Caroll',
+    name: 'Angela Caroll',
     role: 'Chief Editor',
     propic: 'angela-caroll-chief-editor.jpg',
 };
 
 const walterGordon = {
-    staffName: 'Walter Gordon',
+    name: 'Walter Gordon',
     role: 'Office Manager',
     propic: 'walter-gordon-office-manager.jpg',
 };
 
 const angelaLopez = {
-    staffName: 'Angela Lopez',
+    name: 'Angela Lopez',
     role: 'Social Media Manager',
     propic: 'angela-lopez-social-media-manager.jpg',
 };
 
 const scottEstrada = {
-    staffName: 'Scott Estrada',
+    name: 'Scott Estrada',
     role: 'Developer',
     propic: 'scott-estrada-developer.jpg',
 };
 
 const barbaraRamos = {
-    staffName: 'Barbara Ramos',
+    name: 'Barbara Ramos',
     role: 'Graphic Designer',
+    propic: 'barbara-ramos-graphic-designer.jpg',
+};
+
+const gordonFreeman = {
+    name: 'Gordon Freeman',
+    role: 'theoretical physicist',
     propic: 'barbara-ramos-graphic-designer.jpg',
 };
 
 
 
+/*sotto per richiamare un attributo di un oggetto: scrivere nome ogetto seguito da punto e attributo (dot notation), 
+possibile concatenare più attributi usando la virgola
+(gli apici con gli spazi servono ad inserire un distanziamento per rendere piu leggibile in console)*/
+console.log(angelaLopez.role, '     ', angelaLopez.propic);
 
-console.log(angelaLopez.role);
-
-
-const staffInfo = [wayneBarnett, angelaCaroll, walterGordon, angelaLopez, scottEstrada, barbaraRamos];
+// array (lista) contentente gli ogetti
+const staffInfo = [wayneBarnett, angelaCaroll, walterGordon, angelaLopez, scottEstrada, barbaraRamos, gordonFreeman];
 console.log(staffInfo);
+// per aggiungere un attributo a un ogetto
+gordonFreeman.custom = "TRUE";
